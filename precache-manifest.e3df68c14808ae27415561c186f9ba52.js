1adac2b005105d5fa94d9f6ff9f413b38dd138e0
Eddy Collado <eddcollado@gmail.com>
2020-07-06 16:44:33 -0600

    Updates

---
+++ precache-manifest.e3df68c14808ae27415561c186f9ba52.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "df465aedab4bbeb846d6",
+    "url": "/myreactport/static/css/main.91bbb71c.chunk.css"
+  },
+  {
+    "revision": "df465aedab4bbeb846d6",
+    "url": "/myreactport/static/js/main.df465aed.chunk.js"
+  },
+  {
+    "revision": "8aef0bf112849fd13a64",
+    "url": "/myreactport/static/js/runtime~main.8aef0bf1.js"
+  },
+  {
+    "revision": "4885b37ec1618b4c77f9",
+    "url": "/myreactport/static/js/2.4885b37e.chunk.js"
+  },
+  {
+    "revision": "d4dff809f6e1b1cf52ba02b1be1da1d4",
+    "url": "/myreactport/index.html"
+  }
+];
