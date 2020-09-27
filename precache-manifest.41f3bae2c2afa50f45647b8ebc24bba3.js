86842c6642ede6772015dac7db80fea837d64e33
Eddy Collado <eddcollado@gmail.com>
2020-09-27 16:02:34 -0600

    Updates

--- precache-manifest.41f3bae2c2afa50f45647b8ebc24bba3.js
+++ precache-manifest.41f3bae2c2afa50f45647b8ebc24bba3.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = [
   {
-    "revision": "b0095314f19f33649f9b",
+    "revision": "5645de8c54c934636a91",
     "url": "/myreactport/static/css/main.f6d6e2a7.chunk.css"
   },
   {
-    "revision": "b0095314f19f33649f9b",
-    "url": "/myreactport/static/js/main.b0095314.chunk.js"
+    "revision": "5645de8c54c934636a91",
+    "url": "/myreactport/static/js/main.5645de8c.chunk.js"
   },
   {
     "revision": "8aef0bf112849fd13a64",
@@ -16,7 +16,7 @@ self.__precacheManifest = [
     "url": "/myreactport/static/js/2.4885b37e.chunk.js"
   },
   {
-    "revision": "3af5b7a1574409b1ff5a409894cfa66c",
+    "revision": "bf80944ca094c07136a825362d969292",
     "url": "/myreactport/index.html"
   }
 ];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/myreactport/precache-manifest.246a2de36a3ee16fee9342ec8fefdc01.js"
+  "/myreactport/precache-manifest.41f3bae2c2afa50f45647b8ebc24bba3.js"
 );
 
 workbox.clientsClaim();
